0eb95b2ef2358662b4b165e5b42597b7f543669c
Thibault Mercier <thib.mercier@ymail.com>
2023-03-28 10:06:43 +0200

    ok

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -4,4 +4,5 @@ class Bookmark < ApplicationRecord
   validates :comment, length: { minimum: 6 };
   validates :movie_id, uniqueness: { scope: :list_id }
 
+  has_many :bookmarks, dependent: :destroy
 end
